97adfae6b5b1928ddd55a16901f59391b3295a41
MaksGinger <maksimilianyak@gmail.com>
2024-07-26 16:58:47 +0200

    consider alpha channel

--- lib/compress_png.dart
+++ lib/compress_png.dart
@@ -14,8 +14,34 @@ void compressPng({
     throw Exception('Unable to load image: $inputFilePath');
   }
 
-  final quantizedImage = quantize(image, numberOfColors: colorDepth.value);
+  final quantizedImage =
+      quantizeImageWithAlpha(image, numberOfColors: colorDepth.value);
   File(outputFilePath).writeAsBytesSync(encodePng(quantizedImage));
 
   print('Image was successfully compressed and saved to $outputFilePath');
+}
+
+Image quantizeImageWithAlpha(Image srcImage, {required int numberOfColors}) {
+  final width = srcImage.width;
+  final height = srcImage.height;
+
+  final indexedImage = Image(width: width, height: height, numChannels: 4);
+
+  final quantized = quantize(srcImage, numberOfColors: numberOfColors);
+
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      final srcPixel = srcImage.getPixel(x, y);
+      final quantizedPixel = quantized.getPixel(x, y);
+
+      final r = quantizedPixel.r;
+      final g = quantizedPixel.g;
+      final b = quantizedPixel.b;
+      final a = srcPixel.a;
+
+      indexedImage.setPixelRgba(x, y, r, g, b, a);
+    }
+  }
+
+  return indexedImage;
 }
